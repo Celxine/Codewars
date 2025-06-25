4fc3440bada3ae8cc3e2ea2bef52ee33cfcc0bd1
Celine Ingabire <ingabireceline54@gmail.com>
2025-06-25 11:54:03 +0200

    Add solution - CodeHub

---
+++ 7-kyu/the-first-non-repeated-character-in-a-string/the-first-non-repeated-character-in-a-string.js
@@ -0,0 +1,15 @@
+function firstNonRepeated(s) {
+  
+  let charCount = {};
+  for (let char of s) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+  
+  for (let char of s) {
+    if (charCount[char] === 1) {
+      return char;
+    }
+  }
+  return null;
+}
+​
