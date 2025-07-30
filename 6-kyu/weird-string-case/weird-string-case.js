949e07a049a405648787d712844d784553fbfc96
Celine Ingabire <ingabireceline54@gmail.com>
2025-07-30 16:29:49 +0200

    Add solution - CodeHub

---
+++ 6-kyu/weird-string-case/weird-string-case.js
@@ -0,0 +1,6 @@
+function toWeirdCase(string) {
+    return string.split(' ').map(word => { return word.split('').map((char, index) => {
+           return index % 2 === 0 ? char.toUpperCase() : char.toLowerCase();
+        }).join('');
+    }).join(' ');
+}
